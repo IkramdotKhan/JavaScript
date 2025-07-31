27cbd92942641cd18b024673b609517d812a0855
Ikram Khan <ikramdotkhan@gmail.com>
2025-07-31 15:04:18 +0500

    javascript assignment

---
+++ app.js
@@ -0,0 +1,29 @@
+//  1. SCRIPT TO GREET A WEBSITE VISITOR WITH ALERT BOX
+
+// window.alert("Welcome to our website! We're glad you're here.")
+
+
+// 2.
+
+// window.alert("Error! Please enter a valid password.")
+
+
+// 3.
+
+// window.alert("Welcome to JS land...\nHappy Coding!")
+
+// 4.
+
+// window.alert("Welcome to JS land...")
+// window.alert("Happy Coding")
+
+// 5.done through web browser console
+
+// 6. done thorugh existing facebook clone website 
+
+
+
+
+
+
+
