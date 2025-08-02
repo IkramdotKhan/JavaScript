3b347528b68e51ea47818ecfc435ad9bd1492f21
Ikram Khan <ikramdotkhan@gmail.com>
2025-08-03 02:43:18 +0500

    changed name as per assigned task

---
+++ JavaScript - Assignments/Assignments.js
@@ -0,0 +1,91 @@
+// //  1. SCRIPT TO GREET A WEBSITE VISITOR WITH ALERT BOX
+
+// window.alert("Welcome to our website! We're glad you're here.")
+
+
+// // 2.
+
+// window.alert("Error! Please enter a valid password.")
+
+
+// // 3.
+
+// window.alert("Welcome to JS land...\nHappy Coding!")
+
+// // 4.
+
+// window.alert("Welcome to JS land...")
+// window.alert("Happy Coding")
+
+// // 5.done through web browser console
+
+// // 6. done thorugh existing facebook clone website 
+
+
+
+// // ----------------CHAPTER 2-----------------
+
+
+
+// var username =()
+
+// var myName=("Ikram Khan")
+
+// var message =("Hello World")
+
+// window.alert(message)
+
+// var studentBioDataFormName = ("Ikram Khan")
+// var studentBioDataFormAge = ("24 years old")
+// var studentBioDataFormRole = ("Certified Mobile Application Development")
+// window.alert(studentBioDataFormName)
+// window.alert(studentBioDataFormAge)
+// window.alert(studentBioDataFormRole)
+
+// var pizza = ("pizza\npizz\npiz\npi\np")
+// window.alert(pizza)
+
+
+// var email = ("Ikramdotkhan@gmail.com")
+// window.alert("My Email address is " + email)
+
+// var book = "A smarter way to learn JavaScript"
+// window.alert("I am trying to learn from the book " + book)
+
+// var age = (24)
+// window.alert("I am  "  + age + " Years Old")
+
+
+
+// ----------------------------------- CHAPTER 4 -------------------------------------
+
+// QUESTION NO 1
+
+//  var myFirstName , myLastName , myAge        DECLARING THREE VARIABLE IN A SINGLE STATEMENT 
+// console.log(myFirstName,myLastName,myAge)
+
+// QUESTION NO 2
+
+// var $dollar , _myName , myName , myName60 , my60nAME         // LEGAL NAMES JS 
+// console.log($dollar,_myName,my60nAME,myName60,myName)
+// var 1name , *pet25 , .name , 88name , @name                 // ILLEGAL NAMES IN JS 
+
+// QUESTION NO 3
+
+// check Assignment.HTML for  Question No 3
+
+
+// ----------------------------------- CHAPTER 5 -------------------------------------
+
+
+// QUESTION NO 1
+
+
+let value1 = 5;
+let value2 = 5;
+let totalsum = value1 + value2;
+
+document.write("<h3>The Sum of " + value1 + " and " + value2 + " is " + totalsum + ".</h3>");
+
+
+
