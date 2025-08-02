480cf16e9808eec9a19c13ed6de06ff90b5c0d72
Ikram Khan <ikramdotkhan@gmail.com>
2025-08-03 00:22:22 +0500

    Moved files into 'JavaScript - Assignments' subfolder

---
+++ JavaScript - Assignments/app.js
@@ -0,0 +1,56 @@
+//  1. SCRIPT TO GREET A WEBSITE VISITOR WITH ALERT BOX
+
+window.alert("Welcome to our website! We're glad you're here.")
+
+
+// 2.
+
+window.alert("Error! Please enter a valid password.")
+
+
+// 3.
+
+window.alert("Welcome to JS land...\nHappy Coding!")
+
+// 4.
+
+window.alert("Welcome to JS land...")
+window.alert("Happy Coding")
+
+// 5.done through web browser console
+
+// 6. done thorugh existing facebook clone website 
+
+
+
+// ----------------CHAPTER 2-----------------
+
+
+
+var username =()
+
+var myName=("Ikram Khan")
+
+var message =("Hello World")
+
+window.alert(message)
+
+var studentBioDataFormName = ("Ikram Khan")
+var studentBioDataFormAge = ("24 years old")
+var studentBioDataFormRole = ("Certified Mobile Application Development")
+window.alert(studentBioDataFormName)
+window.alert(studentBioDataFormAge)
+window.alert(studentBioDataFormRole)
+
+var pizza = ("pizza\npizz\npiz\npi\np")
+window.alert(pizza)
+
+
+var email = ("Ikramdotkhan@gmail.com")
+window.alert("My Email address is " + email)
+
+var book = "A smarter way to learn JavaScript"
+window.alert("I am trying to learn from the book " + book)
+
+var age = (24)
+window.alert("I am  "  + age + " Years Old")
