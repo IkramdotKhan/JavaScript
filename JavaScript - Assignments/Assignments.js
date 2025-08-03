3913c5e5b184ab1a54b4f2375e0ef4293546e8f4
Ikram Khan <ikramdotkhan@gmail.com>
2025-08-04 01:51:11 +0500

    Completed JS Assigments from chapters 3–5

--- JavaScript - Assignments/Assignments.js
+++ JavaScript - Assignments/Assignments.js
@@ -81,11 +81,110 @@
 // QUESTION NO 1
 
 
-let value1 = 5;
-let value2 = 5;
-let totalsum = value1 + value2;
+// let value1 = 5;
+// let value2 = 5;
+// let totalsum = value1 + value2;
 
-document.write("<h3>The Sum of " + value1 + " and " + value2 + " is " + totalsum + ".</h3>");
+// document.write("<h3>The Sum of " + value1 + " and " + value2 + " is " + totalsum + ".</h3>");
+
+// QUESTION NO 2
+
+// let value1 = 9 ;
+// let value2 = 3 ;
+// let subtractionOfvalue1andvalue2 = value1 - value2 ;
+// document.write(" <h3> The Subtraction of " + value1 +  " and " + value2 + " is " + subtractionOfvalue1andvalue2 + ".</h3>");
+
+// let value1 = 9 ;
+// let value2 = 3 ;
+// let multiplicationOfvalue1andvalue2 = value1 * value2 ;
+// document.write(" <h3> The Multiplication of " + value1 +  " and " + value2 + " is " + multiplicationOfvalue1andvalue2 + ".</h3>");
+
+// let value1 = 9 ;
+// let value2 = 3 ;
+// let divisionOfvalue1andvalue2 = value1 / value2 ;
+// document.write(" <h3> The Division of " + value1 +  " and " + value2 + " is " + divisionOfvalue1andvalue2 + ".</h3>");
+
+// let value1 = 9 ;
+// let value2 = 3 ;
+// let modulusOfvalue1andvalue2 = value1 % value2 ;
+// document.write(" <h3> The modulus of " + value1 +  " and " + value2 + " is " + modulusOfvalue1andvalue2 + ".</h3>");
+
+// QUESTION NO 3 (a)
+
+// var value = 52 ;
+
+// QUESTION NO 3 (b)
+
+// document.write("<h3> Value after variable decleration is " + value + ".</h3>")
+
+// QUESTION NO 3 (c , d ,e, f, g , h , i ,  j , k , l)
+
+// let  value = 5 + 7 ;
+//      value++  ;
+
+// document.write("<h3> Value after Incriment is  " + value++  + ".</h3>")
+// let  value = 5 ;
+//      value--  ;
+
+// document.write("<h3> Value after decriment is  " + value--  + ".</h3>")
+
+// let  value1 = 12 ;
+// let  value2 = 3  ;
+
+//     remainderOfValue1andValue2= (value1%value2)
+
+
+// document.write("<h3>The Reminder is : " + remainderOfValue1andValue2  + " .</h3>")
+
+// QUESTION NO 4
+
+// let ticketprice = 600 ;
+// noOfTickets = 5 ;
+
+// costOfFiveTickets = ticketprice * noOfTickets ; 
+
+// document.write("<h3>Total cost to buy " + noOfTickets + " tickets to a movie is " + costOfFiveTickets + ".</h3>")
+
+//---------------------------------------------------------------------------------------------------------------
+
+// QUESTION NO 5  Table Multiplication using for Loop
+
+
+
+// let tableNo = prompt("Please Enter any digit for which the table is required") ;
+//     document.write("<h3> Multiplication Table of " + tableNo + " is " + ".</h3>")
+//     for ( let  i = 1 ; i <= 10 ; i ++ ){
+//     document.write ( tableNo + " x " +  i + " = " + ( tableNo * i ) + " <br> ")
+//     }
+
+//----------------------------------------------------------------------------------------------------------------
+
+// QUESTION NO 6
+
+let celsiusTemp = prompt("Please enter your desired number for which the conversion from " + " °C "  +  "is required ." ) ;
+
+Fehrenheitfromcelsiustemp = (celsiusTemp * 9/5) + 32 ;
+
+document.write(" <h3> " +  celsiusTemp + " &deg;C is " + Fehrenheitfromcelsiustemp + " &deg;F " + " . </h3>")
+
+
+let fehrenheit = prompt( " Please enter digit OF °F TO convert it into °C ") ;
+    fehrenheit = Number(fehrenheit);
+
+ let   celsiusfromfehrenheit = (fehrenheit - 32) * 5/9; 
+    document.write("<h3>" + fehrenheit + "&deg;F is " + celsiusfromfehrenheit.toFixed(3) + "&deg;C" + ". </h3>")
+
+//---------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 
 
 
