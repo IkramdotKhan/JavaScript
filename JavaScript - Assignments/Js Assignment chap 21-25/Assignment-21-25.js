dbbcaba2f82ee2c8f5c8a042895c1e77aabae368
Ikram Khan <ikramdotkhan@gmail.com>
2025-08-27 00:46:51 +0500

    Js Assignment chap 21-25 completed.

---
+++ JavaScript - Assignments/Js Assignment chap 21-25/Assignment-21-25.js
@@ -0,0 +1,121 @@
+// Question No-1 : Write a program that takes two user inputs for first and last name using prompt and merge them in a new variable titled fullName. Greet the user using his full name .
+
+/*
+let firstName = prompt('Please enter your first name ?')
+let lastName = prompt('Please enter your last name ?')
+
+fullName = firstName + ' ' +  lastName
+
+console.log(fullName)
+alert(`Hey ${fullName} welcome to the club`)
+*/
+//_______________________________________________________________________________________________________________________________________
+
+// Question No-2 : Write a program to take a user input about his favorite mobile phone model.Find and display the length of user input in your broweser .
+
+/*
+let userinput = prompt('Hey , what is your favourite mobile phone model ?')
+userinputlength = userinput.length
+document.write(`Length of string : ${userinputlength}`)
+console.log(userinputlength)   // 5
+*/
+//_______________________________________________________________________________________________________________________________________
+
+// Question No-3 : Write a program to find the index of letter "n" in the word "pakistani" and display the result in your browser.
+/*
+
+const givenName = 'Pakistani' ; 
+let indexofn = givenName.indexOf('n');
+console.log(`Index of n : ${indexofn}`) 
+
+*/
+
+//_______________________________________________________________________________________________________________________________________
+
+// Question No-4 : Write a program to find the last index of letter "l" in the word "Hello World" and display the result in your browser.
+
+/*
+const givenWord=("Hellow World") ; 
+  let lastIndexOfGivenWord = givenWord.lastIndexOf('l')
+  console.log(lastIndexOfGivenWord) //10
+
+*/
+
+//_______________________________________________________________________________________________________________________________________
+/*
+Question No-5 : Write a program to find the character at 3rd index in the word "Pakistani" and display the result in your browser. */
+/*
+   const givenSting = "Pakistani" ; 
+   console.log(totalLength) //9
+   let thirdIndex = givenSting.charAt(3);
+  console.log(thirdIndex)
+*/
+
+
+
+
+//_______________________________________________________________________________________________________________________________________
+
+/*
+Question No-6 : Repeat Question No 1 using string Concete method() . 
+*/
+/*
+let firstName = 'Ikram'; 
+let lastName = 'Khan';
+const fullname = firstName.concat(' ' , lastName )
+console.log(fullname)
+*/
+//_______________________________________________________________________________________________________________________________________
+
+/* Question No - 7 : Write a program to replace the "hyder" to "islam" in the word "hyderabad" and display the result in your browser . 
+*/
+
+/*
+let givenword = 'hyderabad' ;
+updatedword ='Islam' +  givenword.slice(5) 
+console.log(updatedword)
+*/
+
+//_______________________________________________________________________________________________________________________________________
+
+/*
+Question No-8 : Write a program to replace all occurrences of "and" int he string "&" and display the result in your browser . 
+"Ali and sami are best friends.they play cricket football together"
+*/
+
+//   var givenString = "Ali and sami are  or best or  friends.they play cricket and my  football my  together" ;
+
+// //   let replacedstring = givenString.replaceAll('and' , '&')
+
+// // console.log(replacedstring)
+
+
+//_______________________________________________________________________________________________________________________________________
+
+/*
+Question No-9 : 
+*/
+
+
+
+
+
+
+
+//_______________________________________________________________________________________________________________________________________
+
+/*
+Question No-10 : 
+*/
+
+
+
+
+
+
+
+
+// X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-X-
+
+
+
